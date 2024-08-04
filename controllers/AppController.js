d58c2ae715ee498776c6081f39e78b91e2bf8fc7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-04 23:29:58 +0100

    controller

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import redisClient from "../utils/redis.js";
+import dbClient from "../utils/db.js";
+
+class AppController {
+  static getStatus() {
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+
+    return { redis, db };
+  }
+
+  static async getStats() {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    return { users, files };
+  }
+}
+
+export default AppController;
