80b548729cf7e46933c7415a002f6ee16c3ed53f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-04 20:58:52 +0100

    db client

--- utils/db.js
+++ utils/db.js
@@ -5,6 +5,8 @@ class DBClient {
     this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || 27017;
     this.database = process.env.DB_DATABASE || 'files_manager';
+
+    const client = mongodb.
   }
 
   isAlive() {
