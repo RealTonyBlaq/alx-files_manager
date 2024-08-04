647de25fae2475844c36dd168a7bf82a45b1a0c4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-04 20:49:43 +0100

    db client

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,6 @@ class DBClient {
   constructor() {
     this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || ''
+    this.database = process.env.DB_DATABASE || 'files_manager';
   }
 }
