aace3d76f3b1690b97e9b1cd76b47ae7ed4c1878
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-04 21:02:35 +0100

    db client

--- utils/db.js
+++ utils/db.js
@@ -2,11 +2,11 @@ import MongoClient from 'mongodb';
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-    const client = MongoClient(`mongodb://${this}:${}`);
+    const client = MongoClient(`mongodb://${host}:${port}/${database}`);
   }
 
   isAlive() {
