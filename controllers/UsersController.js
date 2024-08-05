051630ff1ee61deaca4c997809498b36e5475899
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-05 02:23:14 +0100

    fixed lint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/extensions */
-import dbClient from "../utils/db.js";
-
+import dbClient from '../utils/db.js';
 
 class UsersController {
   static async postNew(email, password) {
-    return await dbClient.createUser(email, password);
+    const user = await dbClient.createUser(email, password);
+    return user;
   }
 }
 
