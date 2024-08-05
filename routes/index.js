c3e66591ecef31a86a82698c46dd15fa3bf59ae7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-05 02:12:12 +0100

    added new routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = Router();
 
@@ -10,6 +11,20 @@ router.get('/status', (req, res) => {
 
 router.get('/stats', async (req, res) => {
   res.status(200).send(await AppController.getStats());
+});
+
+router.post('/users', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) res.status(400).send({ error: 'Missing email' });
+  if (!password) res.status(400).send({ error: 'Missing password' });
+
+  try {
+    const response = await UsersController.postNew(email, password);
+    res.status(201).send(response);
+  } catch (err) {
+    res.status(400).send({ error: 'Already exist' });
+  }
 });
 
 export default router;
