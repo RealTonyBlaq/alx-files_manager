8b771d3b3a0257e54292a9d2bacdd62a9890c9d5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-05 02:11:05 +0100

    main

--- 1-main.js
+++ 1-main.js
@@ -1,3 +1,4 @@
+import e from 'express';
 import dbClient from './utils/db.js';
 
 const waitConnection = () => {
@@ -27,4 +28,18 @@ const waitConnection = () => {
     console.log(dbClient.isAlive());
     console.log(await dbClient.nbUsers());
     console.log(await dbClient.nbFiles());
+
+    console.log('\nTesting userExists');
+    console.log(await dbClient.userExists('test'));
+    console.log(await dbClient.userExists('fakeUser'));
+
+    console.log('\nTesting user creation');
+    //console.log('success =>', await dbClient.createUser('ifeanyi@yahoo.com', 'pwd'));
+    try {
+      console.log(await dbClient.createUser('Ifeanyi2', 'pwd'));
+    } catch (err) {
+      console.error(err);
+    }
+    console.log('\nTesting retrieveUser');
+    console.log('GET =>', await dbClient.retrieveUser('ifeanyi@yahoo.com'));
 })();
