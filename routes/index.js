ac2983469e8714f4bac3857fb99d14ad3241e19d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-05 11:13:53 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,24 @@ import UsersController from '../controllers/UsersController.js';
 const router = Router();
 
 router.get('/status', (req, res) => {
-  res.status(200).send(AppController.getStatus());
+  return res.status(200).send(AppController.getStatus());
 });
 
 router.get('/stats', async (req, res) => {
-  res.status(200).send(await AppController.getStats());
+  return res.status(200).send(await AppController.getStats());
 });
 
 router.post('/users', async (req, res) => {
   const { email, password } = req.body;
 
-  if (!email) res.status(400).send({ error: 'Missing email' });
-  if (!password) res.status(400).send({ error: 'Missing password' });
+  if (!email) return res.status(400).send({ error: 'Missing email' });
+  if (!password) return res.status(400).send({ error: 'Missing password' });
 
   try {
     const response = await UsersController.postNew(email, password);
-    res.status(201).send(response);
+    return res.status(201).send(response);
   } catch (err) {
-    res.status(400).send({ error: 'Already exist' });
+    return res.status(400).send({ error: 'Already exist' });
   }
 });
 
