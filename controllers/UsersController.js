34e6c667692741eef90996449783df2f7657d890
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-06 22:54:21 +0100

    fixed a bug

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/extensions */
 import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 
 class UsersController {
   static async postNew(email, password) {
@@ -8,9 +9,14 @@ class UsersController {
   }
 
   static async getMe(req) {
-    const token = req.headers['x-token'];
+    const token = req.headers['x-token'] || req.headers['X-Token'];
     if (token) {
-      const user = await dbClient.retrieveUser({ token });
+      const key = `auth_${token}`;
+      const _id = await redisClient.get(key);
+
+      if (!_id) throw new Error('Incorrect token');
+
+      const user = await dbClient.retrieveUser({ _id });
       return user;
     }
     throw new Error('Incorrect Auth format');
